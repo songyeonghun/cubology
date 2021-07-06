9ba9e9dc36f49321687f9547a98aceeaa7d11562
hyeanju <86582031+hyeanju@users.noreply.github.com>
2021-07-06 17:00:18 +0900

    day 7-2
    메인
    옵션
    스테이지 선택 화면
    미완

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject menuselectpoint1;
+    public GameObject menuselectpoint2;
+    public string Nextscene;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,23 @@ public class Menu : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+
+    void OnMouseEnter()
     {
-        
+        menuselectpoint1.SetActive(true);
+        menuselectpoint2.SetActive(true);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("»Æ¿Œ");
+            SceneManager.LoadScene(Nextscene);
+        }
+
+    }
+    void OnMouseExit() 
+    {
+        menuselectpoint1.SetActive(false);
+        menuselectpoint2.SetActive(false);
     }
 }
