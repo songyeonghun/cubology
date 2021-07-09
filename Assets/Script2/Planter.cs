cbd11c4a73645fbc6aea753336272fcd44d1dcf3
20181181송영훈 <20181181@vision.hoseo.edu>
2021-07-09 16:59:49 +0900

    day10-3
    플렌터 틀완성

--- Assets/Script2/Planter.cs
+++ Assets/Script2/Planter.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Planter : MonoBehaviour
 {
-    public GameObject SeedPrefeb, BullletSpawn;
-
+    public GameObject SeedPrefeb, BullletSpawn, BullletSpawn1, BullletSpawn2;
+    int work = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Spark")
+        if (work==0&&collision.gameObject.tag == "Spark")
         {
-            var bullet = Instantiate(SeedPrefeb, BullletSpawn.transform.position, Quaternion.identity).GetComponent<Bullet>();
+            var bullet = Instantiate(SeedPrefeb, BullletSpawn.transform.position, Quaternion.identity).GetComponent<Seed>();
             bullet.Fire(transform.right);
-            bullet = Instantiate(SeedPrefeb, BullletSpawn.transform.position, Quaternion.identity).GetComponent<Bullet>();
+            bullet = Instantiate(SeedPrefeb, BullletSpawn1.transform.position, Quaternion.identity).GetComponent<Seed>();
             bullet.Fire(transform.right);
-            bullet = Instantiate(SeedPrefeb, BullletSpawn.transform.position, Quaternion.identity).GetComponent<Bullet>();
+            bullet = Instantiate(SeedPrefeb, BullletSpawn2.transform.position, Quaternion.identity).GetComponent<Seed>();
             bullet.Fire(transform.right);
+            work=1;
         }
     }
 
--- Assets/Script2/Seed.cs
+++ Assets/Script2/Seed.cs
@@ -24,6 +24,6 @@ public class Seed : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        Destroy(gameObject);
     }
 }
