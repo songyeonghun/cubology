6fb549da9d469121d64a8aa4d9b87eb28e1b0fa2
hyeanju <86582031+hyeanju@users.noreply.github.com>
2021-07-16 11:56:18 +0900

    day 15-1

--- Assets/Script2/LeverObj.cs
+++ Assets/Script2/LeverObj.cs
@@ -15,7 +15,6 @@ public class LeverObj : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collision");
         if (collision.gameObject.tag == "Player")
         {
             onoff++;
