7b8c902e2d9f3486a937477cb58aa61985f8568e
hyeanju <86582031+hyeanju@users.noreply.github.com>
2021-07-22 16:59:16 +0900

    day 19-1

---
+++ Assets/Script3/Eleclay.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eleclay : MonoBehaviour
+{
+    public GameObject laser;
+    public GameObject eleclay;
+    public GameObject elecston;
+    float cubeDistance;
+    int onoff = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Spark")
+        {
+            if (eleclay.transform.position.y = elecston.transform.position.y)
+            {
+                cubeDistance = Vector3.Distance(eleclay.transform.position, elecston.transform.position);
+                if (cubeDistance < 9)
+                {
+                    for (int i = 1; i - 1 < cubeDistance; i++)
+                    {
+                        Instantiate(laser, new Vector3(transform.position.x + i, transform.position.y, 0), Quaternion.identity);
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Instantiate(laser, new Vector3(transform.position.x + i, transform.position.y, 0), Quaternion.identity);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 1; i < 9; i++)
+                {
+                    Instantiate(laser, new Vector3(transform.position.x + i, transform.position.y, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Destroy(laser);
+    }
+}
