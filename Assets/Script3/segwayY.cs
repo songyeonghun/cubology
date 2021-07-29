bd52ac73b142252521b7c9ed589d1e1f0aadde29
hyeanju <86582031+hyeanju@users.noreply.github.com>
2021-07-29 16:37:50 +0900

    day 24-1

--- Assets/Script3/segwayY.cs
+++ Assets/Script3/segwayY.cs
@@ -28,7 +28,7 @@ public class segwayY : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         speed = -speed;
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.tag == "Wall" && collision.gameObject.tag!="Enemy" && collision.gameObject.tag!="Flame")
         {
             distance++;
         }
