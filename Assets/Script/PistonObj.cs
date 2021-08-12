8d564a5a2231f85b8dd980ee4d6c13df8b042463
hyeanju <86582031+hyeanju@users.noreply.github.com>
2021-08-12 16:17:57 +0900

    day 34-2

--- Assets/Script/PistonObj.cs
+++ Assets/Script/PistonObj.cs
@@ -7,18 +7,27 @@ public class PistonObj : MonoBehaviour
     public int onoff;
     float onDelay = 0.2f, timer = 0;
     public Sprite On, Off;
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    void Start()
+    {
+        audioSource1 = GetComponent<AudioSource>();
+        audioSource2 = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
         if (onoff % 2 == 0)
         {
+            audioSource1.Play();
             GetComponent<SpriteRenderer>().sprite = Off;
             GetComponent<BoxCollider2D>().size = new Vector2(0.9f, 0.9f);
             GetComponent<BoxCollider2D>().offset = new Vector2(0f, 0f);
         }
         else
         {
+            audioSource2.Play();
             GetComponent<SpriteRenderer>().sprite = On;
             GetComponent<BoxCollider2D>().size = new Vector2(1.8f, 0.9f);
             GetComponent<BoxCollider2D>().offset = new Vector2(0.5f, 0f);
--- Assets/Script2/ElecStone.cs
+++ Assets/Script2/ElecStone.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class ElecStone : MonoBehaviour
 {
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Spark")
         {
+            audioSource.Play();
             Destroy(gameObject);
         }
     }
